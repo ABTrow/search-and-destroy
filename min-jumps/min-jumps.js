7d5f9156bae3162a09568b33661f1e88d8a87acc
Maksym <mfedorenko16@gmail.com>
2019-09-05 16:07:14 -0400

    mini-jump

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,18 @@
 
 // Complete this algo
 const minJumps = arr => {
+    if (arr[0] >= arr.length - 1) {
+        return 1
+    } 
+    let shortest = Infinity
+    for (let i = 1; i <= arr[0]; i++) {
+       let route = 1 + minJumps(arr.slice(i))
 
+       if (route < shortest) {
+            shortest = route
+       }
+    }
+    return shortest
 };
 
 module.exports = minJumps
