8745b01120f482be542ba96d8fb252e0533190ea
Austin Trow <austintrow@gmail.com>
2019-09-05 15:22:36 -0400

    binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+
+	if (array.length === 0) {
+		return false;
+	} else if (array.length === 1 && array[0] !== target) {
+		return false;
+	}
+
+	array.sort((a,b) => a - b);
+
+	let mid = Math.floor(array.length / 2);
+	let left = array.slice(0, mid);
+	let right = array.slice(mid);
+
+	if (array[mid] === target) {
+		return true;
+	} else if (target < array[mid]) {
+		return binarySearch(left, target);
+	} else if (target > array[mid]) {
+		return binarySearch(right, target);
+	}
+
 };
 
 /*
@@ -14,4 +34,23 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+const extraBinarySearch = (array, target) => {
+	let start = 0;
+	let end = array.length - 1;
+
+	while (start < end) {
+		let mid = Math.floor((start + end) / 2);
+		if (array[mid] === target) {
+			return true;
+		} else if (target < array[mid]) {
+			end = mid - 1;
+		} else if (target > array[mid]) {
+			start = mid + 1;
+		}
+	}
+
+	return false;
+
+};
+
+module.exports = {binarySearch, extraBinarySearch}
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 const {expect} = require('chai');
-const binarySearch = require('./binary-search');
+const {binarySearch, extraBinarySearch} = require('./binary-search');
 
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
@@ -20,6 +20,21 @@ describe('Binary Search', () => {
 		expect(binarySearch(test3, 4)).to.equal(false);
 	});
 
+});
+
 	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
+
+	describe('Extra Binary Search', () => {
+		it('returns true if target value is in array', () =>{
+			expect(extraBinarySearch(test1, 10)).to.equal(true);
+			expect(extraBinarySearch(test2, -3)).to.equal(true);
+			expect(extraBinarySearch(test2, 20)).to.equal(true);
+			expect(extraBinarySearch(test3, 2.6)).to.equal(true);
+		});
+
+		it('returns false if target value is not in array', () => {
+			expect(extraBinarySearch(test2, 25)).to.equal(false);
+			expect(extraBinarySearch(test3, 4)).to.equal(false);
+		});
 
 });
