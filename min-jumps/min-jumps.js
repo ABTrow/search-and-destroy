0c458b7b892a156a8e2ccf5ecb3aef5edc04e005
Austin Trow <austintrow@gmail.com>
2019-09-05 17:45:41 -0400

    better minJumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -3,17 +3,17 @@
 // Complete this algo
 const minJumps = arr => {
     if (arr[0] >= arr.length - 1) {
-        return 1
-    } 
-    let shortest = Infinity
-    for (let i = 1; i <= arr[0]; i++) {
-       let route = 1 + minJumps(arr.slice(i))
+        return 1;
+    }
+    let shortest = Infinity;
+    for (let i = arr[0]; i >= 1; i--) {
+       let route = 1 + minJumps(arr.slice(i));
 
        if (route < shortest) {
-            shortest = route
+            shortest = route;
        }
     }
-    return shortest
+    return shortest;
 };
 
-module.exports = minJumps
+module.exports = minJumps;
